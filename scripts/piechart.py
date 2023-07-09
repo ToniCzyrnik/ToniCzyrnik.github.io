a0d020b1f0a0e533644c75bc6788d017107e5d26
Toni Czyrnik <mail@czyrnik.me>
2023-07-09 09:59:12 +0800

    new description

--- scripts/piechart.py
+++ scripts/piechart.py
@@ -5,9 +5,8 @@ import os
 colours = ["#E63946", "#1D3557", "#457B9D", "#A8DADC"]
 
 # general information
-city = "taipei"
-topic = "electricity"
-#title = "Exports of Taiwan"
+export_folder = "taipei"
+file_name = "electricity"
 
 # declaring data \n
 data = [44.9, 36.4, 9.6]
@@ -17,7 +16,6 @@ data_sum = sum(data)
 data.append(100 - data_sum)
 keys.append("Renewables")
 
-
 # formats
 plt.style.use("dark_background")
 sns.set(style="darkgrid", context="talk", font="Roboto", palette=colours, rc={'axes.facecolor': 'black', 'figure.facecolor': 'black'})
@@ -25,18 +23,17 @@ sns.set(style="darkgrid", context="talk", font="Roboto", palette=colours, rc={'a
 #plt.title(f"{title}", y=1.05, fontsize=20, color="#F1FAEE")
 
 # plotting data on chart
-plot = plt.pie(data, labels=keys, autopct='%.0f%%', textprops={'fontsize': 14, 'color': "#F1FAEE"})
+# autopct: for adding percentages in the figure, ".1f" means one digit
+# textprops: adding labels to the parts
+plot = plt.pie(data, labels=keys, autopct='%.1f%%', textprops={'fontsize': 14, 'color': "#F1FAEE"})
 
-#plot.set(xlabel=None)
-#plot.set(ylabel=None)
-#sns.despine(left=True)
-
-path = f"./export/{city}"
+# export
+path = f"./export/{export_folder}"
 
 if not os.path.exists(path):
     os.makedirs(path)
 
-plt.savefig(f"{path}/{topic}.png", transparent=True, dpi=300,)
+plt.savefig(f"{path}/{file_name}.png", transparent=True, dpi=300,)
 
-# show
+# show figure
 plt.show()
